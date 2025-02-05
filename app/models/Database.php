f1592b561207cb493144d80425e5b6f5b307fb6e
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-02-05 12:08:43 +0100

    changer la database mysql vers postgresq

--- app/models/Database.php
+++ app/models/Database.php
@@ -3,6 +3,7 @@
 namespace App\Core;
 
 use PDO;
+use PDOException;
 
 final class Database
 {
@@ -12,11 +13,17 @@ final class Database
     private function __construct()
     {
         try {
-            $this->connection = new PDO("mysql:host=localhost;dbname=cabinet_medical_mvc", "root", "");
-            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $host = getenv('DB_HOST') ?: 'localhost';   
+            $dbname = getenv('DB_NAME') ?: 'cabinet_medical_mvc'; 
+            $username = getenv('DB_USER') ?: 'root';     
+            $password = getenv('DB_PASSWORD') ?: '';    
+
+            $dsn = "pgsql:host=$host;dbname=$dbname"; 
+            $this->connection = new PDO($dsn, $username, $password);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
             $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        } catch (\PDOException $e) {
-            die("Erreur de connexion à la base de données : " . $e->getMessage());
+        } catch (PDOException $e) {
+            throw new DatabaseException("Erreur de connexion à la base de données : " . $e->getMessage());
         }
     }
 
